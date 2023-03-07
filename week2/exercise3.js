961a254672a40053b6dc5f0a51eeb9fc366eca87
grzianod <grazianodinocca@icloud.com>
2023-03-07 10:19:13 +0100

    v1.0

--- week2/exercise3.js
+++ week2/exercise3.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function Answer(text, respondent, score, date) {
+    this.text = text;
+    this.respondent = respondent;
+    this.score = score;
+    this.date = date;
+}
+
+function Question(question, questioner, date, answerList) {
+
+
+}
+
+const ans1 = new Answer("for of", "Alice", 3, "2023-03-07");
+console.log(ans1);
+
