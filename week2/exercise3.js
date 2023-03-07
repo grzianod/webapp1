13fd272369100fe13dc631497eb5944c4b4aa081
grzianod <grazianodinocca@icloud.com>
2023-03-07 11:06:22 +0100

    v1.0

--- week2/exercise3.js
+++ week2/exercise3.js
@@ -13,13 +13,45 @@ function Answer(text, respondent, score, date) {
     }
 }
 
-function Question(question, questioner, date, answerList) {
-    this.question = question;
+function Question(text, questioner, date) {
+    this.text = text;
     this.questioner = questioner;
     this.date = date;
-    this.answerList = answerList;
-}
+    this.list = [];
 
-const ans1 = new Answer("for of", "Alice", 3, dayjs("2023-03-07"));
-console.log(ans1.str());
+    this.add = function(answer) {
+        this.list.push(answer);
+    }
+
+    this.findAll = function(name) {
+        return this.list.filter(answer => answer.respondent == name);
+    }
+
+    this.afterDate = function(date) {
+        return this.list.filter(answer => answer.date.isAfter(date));
+    }
+
+    this.listByDate = function() {
+        const newList = [...this.list];
+        return newList.sort((a,b) => a.date.diff(b.date));
+    }
+
+    this.listByScore = function() {
+        const newList = [...this.list];
+        return newList.sort((a,b) => b.score - a.score);
+    }
+
+}
+let quest = new Question("Best way of enumerating an array in JS?", "Bob", dayjs("2023-02-28"));
+quest.add(new Answer("for of", "Alice", 3, dayjs("2023-03-07")));
+quest.add(new Answer("for in", "Chuck", 5, dayjs("2023-03-01")));
+quest.add(new Answer("for (i=0; i<N; i++)", "Daria", 4,dayjs("2023-03-02")));
+quest.add(new Answer("while(i<N)", "Elly", 1, dayjs("2023-03-04")));
+
+let find = quest.findAll("Alice");
+let after = quest.afterDate("2023-03-02");
+let sortByDate = quest.listByDate();
+let sortByScore = quest.listByScore();
+sortByDate.forEach(e => console.log(e));
+
 
