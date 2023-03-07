16f7517b0c3d1644fa21d6aaf40f841b9dab56f1
grzianod <grazianodinocca@icloud.com>
2023-03-07 10:27:01 +0100

    v1.0

--- week2/exercise3.js
+++ week2/exercise3.js
@@ -1,7 +1,8 @@
 "use strict";
 const dayjs = require('dayjs');
+let now = dayjs().format();
 
-function Answer(text, respondent, score, date) {
+function Answer(text, respondent, score, dayjs: date) {
     this.text = text;
     this.respondent = respondent;
     this.score = score;
@@ -9,10 +10,13 @@ function Answer(text, respondent, score, date) {
 }
 
 function Question(question, questioner, date, answerList) {
-
-
+    this.question = question:
+    this.questioner = questioner;
+    this.date = date;
+    this.answerList = answerList;
 }
 
 const ans1 = new Answer("for of", "Alice", 3, "2023-03-07");
 console.log(ans1);
+console.log(now);
 
